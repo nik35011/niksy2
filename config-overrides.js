c21ea6cb1e41f959cb8f321b5751dc94faf3b458
nik35011 <119336298+nik35011@users.noreply.github.com>
2024-01-14 18:02:06 +0530

    Add files via upload

---
+++ config-overrides.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+
+module.exports = function override(config) {
+  if (!config.resolve.fallback) {
+    config.resolve.fallback = {};
+  }
+
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    "crypto": require.resolve("crypto-browserify"),
+    "stream": require.resolve("stream-browserify"),
+    "assert": require.resolve("assert"),
+    "http": require.resolve("stream-http"),
+    "https": require.resolve("https-browserify"),
+    "os": require.resolve("os-browserify"),
+    "url": require.resolve("url"),
+    "zlib": false,
+    "net": false,
+    "fs": false,
+    "dns": false,
+    "Buffer": false,
+    "process": false,
+  };
+
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ]);
+
+  return config;
+};
